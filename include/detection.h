bcf3be9b11397e833cfccbd15b46c730e5756330
Maurits Muijsert <MPW.Muijsert@student.han.nl>
2019-03-03 18:56:44 +0100

    doxygen detection

--- include/detection.h
+++ include/detection.h
@@ -1,3 +1,14 @@
+/**
+ * @file detection.h
+ * @author Maurits Muijsert (MPW.Muijsert@student.han.nl), Martijn Vogelaar
+ * @brief function that wrap detection and drawin.
+ * @version 0.1
+ * @date 2019-03-03
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
 #ifndef DETECTION_H
 #define DETECTION_H
 #include "language.h"
@@ -14,19 +25,33 @@ using namespace cv;
  * @brief detect and draw
  * 
  */
-
 class Detection
 {
 private:
   Detection();
   ~Detection();
 
-
   static void detectAndDrawOnce(cv::Mat image, std::atomic<Specification>* spec, std::atomic<bool>* needToPrint, Language language);
 public:
 
+  /**
+   * @brief detects and draws on a live image from the webcam
+   * 
+   * @param exitProgram a variable used to know if the program needs to stop.
+   * @param spec the specification which to detect.
+   * @param camera which is used for getting the live images
+   */
   static void detectAndDrawLive(std::atomic<bool>* exitProgram,std::atomic<Specification>* spec, short camera);
 
+  /**
+   * @brief detects and draws on a static image from a file
+   * 
+   * @param exitProgram 
+   * @param spec a variable used to know if the program needs to stop.
+   * @param needToPrint status if it is needed to print
+   * @param imageFileName the name of the desired image.
+   * @param language the language the feedback is printed in.
+   */
   static void detectAndDrawStatic(std::atomic<bool>* exitProgram, std::atomic<Specification>* spec, std::atomic<bool>* needToPrint, const std::string & imageFileName, Language language);
 
 };
